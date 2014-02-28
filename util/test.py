f1bfd9cdde3acf7e0c62157290d14cddf08ebb6c
Thomas Thornton <thomasx.l.thornton@intel.com>
2014-02-28 14:22:39 -0700

    added a couple utility functions

---
+++ util/test.py
@@ -0,0 +1,9 @@
+class Test:
+    def hello(self, x):
+        return "hello " + x
+
+    def removeFromAllInSeries(self, series, target):
+        return [x.strip(target) for x in series]
+
+    def firstFromRange(self, series):
+        return [x.split('-')[0] for x in series]
